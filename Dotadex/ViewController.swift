12d3b185b3f36b7e9c0d985e6023094d3fbb78b5
Rizki Dwi Putra <rizki.jtk07@gmail.com>
2020-04-15 14:36:41 +0700

    Initial Commit

---
+++ Dotadex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dotadex
+//
+//  Created by Rizki Dwi Putra on 15/04/20.
+//  Copyright © 2020 Rizki Dwi Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
